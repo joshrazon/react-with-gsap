4148d5cf040abc434642356c0729ccbc99f960dd
Josh <joshrazonn@gmail.com>
2021-01-04 22:52:17 -0700

    updated readme and title

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# GreenSock with React
+# Greensock Animations
 
-## Code Sandbox [Link](https://ozv2f.csb.app/#/)
+Interactive animation visualizer where users can change properties of the animation and see the effects immediately.
+
+
+
+## Tools Used
+
+I made this project using [React](https://reactjs.org/) and [GreenSock](https://greensock.com/) libraries and deployed to [Heroku](https://www.heroku.com/).
+
+## Make your own copy
+
+```bash
+git clone https://github.com/joshrazon/react-with-gsap.git
+
+cd react-with-gsap
+
+npm i # installs dependencies
+
+...
+
+npm start # runs devevelopment server on localhost:3000
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import React from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 import Animation1 from "./Animation1";
 import Stuff from "./Stuff";
 import Animation3 from "./Animation3";
@@ -13,12 +9,22 @@ function App() {
   return (
     <HashRouter>
       <div>
-        <h1>GreenSock with React</h1>
+        <h1 className="attribution">Green<span className="sock">Sock</span> Animations</h1>
         <ul className="header">
-          <li><NavLink to="/" exact>Animation 1</NavLink></li>
-          <li><NavLink to="/stuff">Animation 2</NavLink></li>
-          <li><NavLink to="/animation3">Animation 3</NavLink></li>
-          <li><NavLink to="/animation4">Animation 4</NavLink></li>
+          <li>
+            <NavLink to="/" exact>
+              Animation 1
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/stuff">Animation 2</NavLink>
+          </li>
+          <li>
+            <NavLink to="/animation3">Animation 3</NavLink>
+          </li>
+          <li>
+            <NavLink to="/animation4">Animation 4</NavLink>
+          </li>
         </ul>
         <div className="content">
           <Route path="/" component={Animation1} exact />
@@ -27,7 +33,6 @@ function App() {
           <Route path="/animation4" component={Animation4} />
         </div>
       </div>
-      <h1 className="attribution">Powered by Green<span className="sock">Sock</span></h1>
     </HashRouter>
   );
 }
